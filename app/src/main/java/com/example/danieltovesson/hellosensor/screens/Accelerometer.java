787fe5e933d81ec331adfd7d3c895050fdd8d6f9
Daniel Tovesson <daniel.tovesson@me.com>
2018-03-20 19:50:30 +0100

    Add Compass and Accelerometer activities

---
+++ app/src/main/java/com/example/danieltovesson/hellosensor/screens/Accelerometer.java
@@ -0,0 +1,15 @@
+package com.example.danieltovesson.hellosensor.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.danieltovesson.hellosensor.R;
+
+public class Accelerometer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer);
+    }
+}
