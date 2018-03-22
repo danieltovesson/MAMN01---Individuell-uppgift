59dc859d141476260bd2a98e43ba095133b44b67
Daniel Tovesson <daniel.tovesson@me.com>
2018-03-22 11:52:58 +0100

    Start implementation of accelerometer

--- app/src/main/java/com/example/danieltovesson/hellosensor/screens/AccelerometerActivity.java
+++ app/src/main/java/com/example/danieltovesson/hellosensor/screens/AccelerometerActivity.java
@@ -1,15 +1,98 @@
 package com.example.danieltovesson.hellosensor.screens;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.danieltovesson.hellosensor.R;
 
-public class AccelerometerActivity extends AppCompatActivity {
+public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+
+    // Variables
+    private TextView xValueTextView, yValueTextView, zValueTextView;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+    private boolean hasAccelerometerSensor = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
+
+        // Initialize variables
+        xValueTextView = findViewById(R.id.xValueTextView);
+        yValueTextView = findViewById(R.id.yValueTextView);
+        zValueTextView = findViewById(R.id.zValueTextView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startSensors();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopSensors();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used in this example
+    }
+
+    /**
+     * Starts the sensors
+     */
+    private void startSensors() {
+
+        // Try to create accelerometer sensor
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Check if accelerometer sensor was created successfully
+        if (accelerometerSensor == null) {
+            noSensorsAlert();
+        } else {
+            hasAccelerometerSensor = sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    /**
+     * Stops the sensors
+     */
+    private void stopSensors() {
+
+        // Unregister sensors
+        if (hasAccelerometerSensor) {
+            sensorManager.unregisterListener(this, accelerometerSensor);
+        }
+    }
+
+    /**
+     * Shows no sensors alert
+     */
+    private void noSensorsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device does not support the desired sensors")
+                .setCancelable(false)
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
     }
 }
