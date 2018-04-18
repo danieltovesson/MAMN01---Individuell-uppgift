e024637c95dd5b0761d83367c7cbfe7ad5a7a594
Daniel Tovesson <daniel.tovesson@me.com>
2018-04-18 11:40:40 +0200

    Improve low pass filter

--- app/src/main/java/com/example/danieltovesson/hellosensor/helpers/Functions.java
+++ app/src/main/java/com/example/danieltovesson/hellosensor/helpers/Functions.java
@@ -1,5 +1,7 @@
 package com.example.danieltovesson.hellosensor.helpers;
 
+import java.math.BigDecimal;
+
 /**
  * Created by danieltovesson on 2018-03-22.
  */
@@ -7,7 +9,7 @@ package com.example.danieltovesson.hellosensor.helpers;
 public class Functions {
 
     // Variables
-    static final float ALPHA = 0.25f;
+    static final float ALPHA = 0.07f;
 
     /**
      * Uses a low pass filter on the input
@@ -21,8 +23,21 @@ public class Functions {
             return input;
         }
         for (int i = 0; i < input.length; i++) {
-            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+            output[i] = round(output[i] + ALPHA * (input[i] - output[i]), 2);
         }
         return output;
+    }
+
+    /**
+     * Rounds a float value
+     *
+     * @param d            the float value
+     * @param decimalPlace the number of decimals
+     * @return the rounded float value
+     */
+    public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
     }
 }
